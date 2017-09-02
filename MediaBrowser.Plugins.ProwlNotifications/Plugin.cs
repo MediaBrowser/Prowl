7045abacdc9cc8db99f04d9bf38a05815df8658a
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-09-01 23:11:13 -0400

    target .net standard

---
+++ MediaBrowser.Plugins.ProwlNotifications/Plugin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Plugins.ProwlNotifications.Configuration;
+
+namespace MediaBrowser.Plugins.ProwlNotifications
+{
+    /// <summary>
+    /// Class Plugin
+    /// </summary>
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = Name,
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.html"
+                }
+            };
+        }
+
+        private Guid _id = new Guid("577f89eb-58a7-4013-be06-9a970ddb1377");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return "Prowl Notifications"; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get
+            {
+                return "Sends notifications via Prowl Service.";
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static Plugin Instance { get; private set; }
+    }
+}
